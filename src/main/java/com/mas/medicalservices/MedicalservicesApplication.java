74c1ac892f67aaf22a0c087aadc7ad0e6794cfb5
BongoKent <masster011@gmail.com>
2022-03-27 22:51:38 +0200

    Initial commit

---
+++ src/main/java/com/mas/medicalservices/MedicalservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mas.medicalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mas/medicalservices/MedicalservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.medicalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
